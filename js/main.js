1fd13ba3a720e3dfe7134fd7c12a4ed4a57b7091
Vinod Sharma <vinod@Vinods-MacBook-Pro.local>
2019-04-19 15:23:47 +0530

    pindid start on node

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(".menu").click(function(event){
-        event.stopPropagation();
-        $(".side-nav").removeClass('hide-menu').addClass('show-menu');
-    });
-    $('body,html').click(function(e){
-        $('.side-nav').removeClass('show-menu').addClass('hide-menu');
-     });
-});
